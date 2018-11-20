1f0bc0e773db5111ac35eff3f90256aee5e712ea
prindeville91 <prindeville91@gmail.com>
2018-11-20 13:06:57 -0600

    updated and fixed update validator and views for update validator

--- apps/pantry_tracker/views.py
+++ apps/pantry_tracker/views.py
@@ -127,8 +127,11 @@ def update_profile(request,id):
     if request.method=="POST":
         errors = User.objects.update_validator(request.POST)
         if len(errors):
+            print(errors)
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!"*10)
+            print(User.objects.get(id = request.session['user_id']).email)
             request.session['errors']=errors
-            route = '/myaccount/' + str(request.POST['id'])
+            route = '/editProfile/' + str(request.session['user_id'])
             return redirect(route)
         else:
             user = User.objects.get(id=request.session['user_id'])
